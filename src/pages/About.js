179a273cd9d3089d8f96a2eac340027d00e6caf3
qstation <luka.z.ilic1996@gmail.com>
2021-11-17 18:43:39 +0100

    clear some code

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,8 @@
 import React from "react";
-// import { Route } from "react-router";
 
 const About = () => {
     return (
-        // <Route exact path="/about">
-            <div>Hello from about page</div>
-        // </Route>
+        <div>Hello from about page</div>
     )
 }
 
